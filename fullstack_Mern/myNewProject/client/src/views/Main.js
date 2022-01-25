d8f68d46dbf11d83abf209dab1de4abf57760f29
Daniel Adamski <dra2103@gmail.com>
2022-01-25 13:01:05 -0800

    commit

---
+++ fullstack_Mern/myNewProject/client/src/views/Main.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState } from 'react' ;
+import ProductForm from '../components/ProductForm';
+
+
+
+export default () => {
+
+    return (
+        <div>
+            <ProductForm />
+        </div>
+    )
+}
---
+++ fullstack_Mern/myNewProject/server/controllers/product.controller.js
@@ -0,0 +1,13 @@
+const {Product} = require('../models/product.model');
+
+module.exports.index = (req, res) => { 
+    res.json({
+        message: "Hello World"
+    });
+}
+
+module.exports.createProduct = (req, res) => {
+    Product.create(req.body)
+        .then(person => res.json(person))
+        .catch(err=>res.json(err))
+}
